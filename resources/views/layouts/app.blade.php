d26148a58f0a43bc742b3c11f086e24219d04d7e
github <github>
2022-04-06 19:55:24 +0900

    フォーマット作成

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <title>MessageBoard</title>
+</head>
+<body>
+    <div class="header">
+        <a class="header-logo" href="/">MessageBoard</a>
+
+        <div class="header-list">
+            <ul>
+                <li class="nav-item">新規メッセージの投稿</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
